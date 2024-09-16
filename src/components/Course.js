1b15fcffb09c6f7ccc14620cef7f11500e7e45de
unknown <joshuaowolabi03@gmail.com>
2024-09-16 15:13:39 +0100

    added image url

--- src/components/Course.js
+++ src/components/Course.js
@@ -4,7 +4,7 @@ import './Courses.css'; // Create and style this
 const Courses = () => {
   const courses = [
     { title: 'Web Development', description: 'Learn to build modern websites.', thumbnail: 'https://media.istockphoto.com/id/1371339413/photo/co-working-team-meeting-concept-businessman-using-smart-phone-and-digital-tablet-and-laptop.jpg?s=1024x1024&w=is&k=20&c=YdGWQdXQYg-G1H89Q12soaleNQEKH9JpihwvrLdFHeA=' },
-    { title: 'Data Science', description: 'Master data analysis and machine learning.', thumbnail: 'https://media.istockphoto.com/id/1431548795/photo/data-scientists-man-programmer-using-laptop-analyzing-and-development-at-various-information.jpg?s=612x612&w=0&k=20&c=z6XZmvUmb4EXtawB4OfBeBxkhlwBY93f0PupT5ITxW8=' },
+    { title: 'Data Science', description: 'Master data analysis and machine learning.', thumbnail: 'https://builtin.com/sites/www.builtin.com/files/styles/ckeditor_optimize/public/inline-images/Data%20Science%201600x800.jpg' },
   ];
 
   return (
